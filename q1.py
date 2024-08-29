5b10f76b11785b1d903a8a0040792ac016f617d0
prayag719 <2153003@ritindia.edu>
2024-08-29 08:51:44 +0530

    first commit

---
+++ q1.py
@@ -0,0 +1,7 @@
+def area_rectangle(lenght,height):
+    print(f"The area of Rectangle with length {lenght} and height {height} =  {lenght * height} unit sq")
+area_rectangle(30,40)
+
+def area_square(side):
+    print(f"The area of square with Side {side} = {side**2} unit sq")
+area_square(30)
